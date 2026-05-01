11ff5ba3bd07fbdd53c4af9bfbc91b592fa1d4e9
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-01 14:36:37 +0300

    Create ConvertBooleanToString.java

---
+++ java/8kyu/ConvertBooleanToString.java
@@ -0,0 +1,10 @@
+// Kyu: 8
+// Problem: Convert a boolean value to string representation
+// Note: Only valid inputs are given
+
+public class ConvertBooleanToString {
+  public static String convert(boolean b) {
+    return Boolean.toString(b);
+    
+  }
+}
