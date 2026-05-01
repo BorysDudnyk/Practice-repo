6a1d0d1a260eb546653bc9f6ff91b70c61c64180
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-01 15:08:29 +0300

    Add CenturyFromYear class to calculate century

---
+++ java/8kyu/CenturyFromYear.java
@@ -0,0 +1,9 @@
+// Kyu: 8
+// Problem: Calculate century from year
+// Formula: (year + 99) / 100
+
+public class Solution {
+  public static int century(int number) {
+    return (number + 99) / 100;
+  }
+}
