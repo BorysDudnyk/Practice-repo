b567fe8126a6b89957d1d39e398ee6f90b8b3a81
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-01 14:51:05 +0300

    Add method to convert string to uppercase

---
+++ java/8kyu/MakeUpperCase.java
@@ -0,0 +1,9 @@
+// Kyu: 8
+// Problem: Convert string to uppercase
+// Method: String.toUpperCase()
+
+class Upper {
+    public static String makeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
