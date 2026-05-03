f8b870dab26d7ac309a0dab97fc47422dae9e002
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-03 13:27:43 +0300

    Create DoubleInteger.java

---
+++ java/8kyu/fundamentals/DoubleInteger.java
@@ -0,0 +1,12 @@
+// Kyu: 8
+// Problem: Double the given integer
+// Approach: simple multiplication
+
+class Java {
+  public static int doubleInteger(int i) {
+    
+    return i * 2;
+    
+    //return i + i;
+  }
+}
