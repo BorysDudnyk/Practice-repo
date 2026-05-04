eef6c1a6c68ee60185bee2067295f6231a6a6e57
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-04 13:23:52 +0300

    Create StringEndsWith.java

---
+++ java/7kyu/strings/StringEndsWith.java
@@ -0,0 +1,9 @@
+// Kyu: 7
+// Problem: Check if string ends with given substring
+// Method: String.endsWith()
+
+public class Kata {
+    public static boolean solution(String str, String ending) {
+        return str.endsWith(ending);
+    }
+}
