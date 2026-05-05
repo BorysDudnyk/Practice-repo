08b66f0071f80fbce19b431d4d97c404ecb2fe4a
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-05 11:46:59 +0300

    Create YesOrNo.java

---
+++ java/8kyu/fundamentals/YesOrNo.java
@@ -0,0 +1,15 @@
+// Kyu: 8
+// Problem: Convert boolean to "Yes" or "No"
+// Approach: ternary operator
+
+class YesOrNo {
+  public static String boolToWord(boolean b) {
+    /*if(b) {
+      return  "Yes";
+    } else {
+      return  "No";
+    }*/
+    
+    return b ? "Yes" : "No";
+  }
+}
