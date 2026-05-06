f1ae8050acc5fcb17db9632c69a259b24fdf67a3
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-06 16:02:00 +0300

    Create Greeting.java

---
+++ java/8kyu/strings/Greeting.java
@@ -0,0 +1,9 @@
+// Kyu: 8
+// Problem: Return greeting string with given name
+// Approach: string concatenation
+
+public class Kata {
+  public static String greet(String name) {
+    return "Hello, " + name + " how are you doing today?";
+  }
+}
