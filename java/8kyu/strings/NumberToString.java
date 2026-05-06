47c43b1f39f2b1e3a4a819bcf61340ed18aace3c
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-06 16:00:41 +0300

    Create NumberToString.java

---
+++ java/8kyu/strings/NumberToString.java
@@ -0,0 +1,14 @@
+// Kyu: 8
+// Problem: Convert integer to string
+// Method: String.valueOf()
+
+class Kata {
+  public static String numberToString(int num) {
+    /*String str = num + "";
+    return str;*/
+    
+    //return Integer.toString(num);
+    
+    return String.valueOf(num);
+  }
+}
