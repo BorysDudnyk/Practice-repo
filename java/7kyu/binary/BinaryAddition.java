854f6501bfd22e942ef2f0477415b97e6810dda0
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-09 16:10:44 +0300

    Create BinaryAddition.java

---
+++ java/7kyu/binary/BinaryAddition.java
@@ -0,0 +1,11 @@
+// Kyu: 7
+// Problem: Convert sum of two numbers to binary string
+// Method: Integer.toBinaryString()
+
+public class Kata{
+  
+  public static String binaryAddition(int a, int b){
+    
+    return Integer.toBinaryString(a + b);
+  }
+}
