de487d90daff1cefdf804bd0a58208b5cf77e05b
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-10 13:06:45 +0300

    Create ThirdAngleTriangle.java

---
+++ java/8kyu/fundamentals/ThirdAngleTriangle.java
@@ -0,0 +1,10 @@
+// Kyu: 8
+// Problem: Find third angle of triangle
+// Formula: sum of angles in triangle = 180
+
+public class ThirdAngle {
+    public static int otherAngle(int angle1, int angle2) {
+        
+        return 180 - angle1 - angle2;
+    }
+}
