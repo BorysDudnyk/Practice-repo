605890707b89883144585d107a4e5edb1bf840e7
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-10 13:02:23 +0300

    Implement stringToArray method to split string

---
+++ java/8kyu/arrays/StringToArray.java
@@ -0,0 +1,10 @@
+// Kyu: 8
+// Problem: Convert string into array of words
+// Method: split by whitespace
+
+public class Solution {
+
+    public static String[] stringToArray(String s) {
+      return s.split(" ");
+    }
+}
