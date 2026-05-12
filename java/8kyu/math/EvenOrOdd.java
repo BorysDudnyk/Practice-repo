7acc72e645a804375f29adf403ca994a01252037
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-12 15:05:04 +0300

    Create EvenOrOdd.java

---
+++ java/8kyu/math/EvenOrOdd.java
@@ -0,0 +1,12 @@
+// Kyu: 8
+// Problem: Determine whether number is even or odd
+
+public class Kata {
+    public static String evenOrOdd(int number) {
+      if(number % 2 == 0){
+        return "Even";
+      }
+      
+      return "Odd";
+    }
+}
