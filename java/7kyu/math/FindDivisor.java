f0d589a3c6b149ea7409328449e9f8822d2c5620
Borys Dudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2026-05-15 15:43:59 +0300

    Implement divisor counting method in FindDivisor class

---
+++ java/7kyu/math/FindDivisor.java
@@ -0,0 +1,17 @@
+// Kyu: 7
+// Problem: Count number of divisors
+
+public class FindDivisor {
+
+  public long numberOfDivisors(int n) {
+    int count = 0;
+    
+    for(int i = 1; i <= n; i++){
+      if(n % i == 0){
+        count++;
+      }
+    }
+    
+    return count;
+  }
+}
